af627580e7776c1563b4e2cac45119fdeb91c9e9
0xSynesty <antoine.labonne1@gmail.com>
2023-02-02 15:35:15 +0100

    removed copyrights

--- hosts_co.sh
+++ hosts_co.sh
@@ -1,10 +1,10 @@
-ipaddr=`ipconfig getifaddr en0` # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-base=${ipaddr:0:10} # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-for ((i=0;i<=255;i++)); do # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    addr=$base$i # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    if ping $addr -c 1 -W 1 > /dev/null  # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    then  echo  $addr | tee online.txt # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    else # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    echo $addr | tee offline.txt # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-    fi # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
-done # COPYRIGHT ANTOINE LABONNE Le vol est puni de trois ans d'emprisonnement et de 45 000 euros d'amende.
+ipaddr=`ipconfig getifaddr en0` 
+base=${ipaddr:0:10} 
+for ((i=0;i<=255;i++)); do 
+    addr=$base$i 
+    if ping $addr -c 1 -W 1 > /dev/null  
+    then  echo  $addr | tee online.txt 
+    else 
+    echo $addr | tee offline.txt 
+    fi 
+done
